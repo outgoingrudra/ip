3bfc8a5f1bc1286f0b1ec43b941b9da87996845b
Rudra Verma <rudraverma781@gmail.com>
2026-02-17 17:28:03 +0530

    backend-ashing

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,11 +3,14 @@ import { ENV } from "./lib/env.js";
 import path from "path"
 import { ConnectDB } from "./lib/db.js";
 import cors from "cors"
+import {serve} from "inngest/express"
+import {inngest} from "./lib/inngest.js"
 
 
 const app = express()
 app.use(cors({origin : ENV.CLIENT_URL, credentials : true}))
 app.use(express.json())
+app.use("/api/inngest",serve({client :inngest , functions }))
 
 
 const __dirname = path.resolve()
