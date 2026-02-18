c7d119dc8bed4d9d0256d8fc7b57483a0e69ab82
Rudra Verma <rudraverma781@gmail.com>
2026-02-18 17:57:36 +0530

    backend-ashing

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -9,7 +9,7 @@ export const ENV ={
     CLIENT_URL : process.env.CLIENT_URL,
     STREAM_API_SECRET : process.env.STREAM_API_SECRET,
     STREAM_API_KEY :  process.env.STREAM_API_KEY,
-    INGEST_SIGNING_KEY:  process.env.INGEST_SIGNING_KEY,
-    INGEST_EVENT_KEY :  process.env.INGEST_EVENT_KEY,
+    INNGEST_SIGNING_KEY:  process.env.INNGEST_SIGNING_KEY,
+    INNGEST_EVENT_KEY :  process.env.INNGEST_EVENT_KEY,
 }
 
